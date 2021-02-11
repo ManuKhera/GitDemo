a0d95950ecc4463513cf7e421e38e5eb5fdd562e
Manukhara <manukhattra13@gmail.com>
2021-02-10 18:38:14 -0800

    first commit

---
+++ src/test/java/acquickacademic/SeleniumTest.java
@@ -0,0 +1,16 @@
+package acquickacademic;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+@Test
+public void BrowserAUtomation()
+{
+	System.out.println("BrowserAUtomation");
+	}
+@Test
+public void elementsUi()
+{
+System.out.println("elementsUi");	
+}
+}
